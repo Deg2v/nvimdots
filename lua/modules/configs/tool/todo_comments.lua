6f7718fc91875a01f9d9190912d4a24bc8a7e128
Deg2v <1311618499@qq.com>
2023-03-06 19:14:21 +0800

    adjust todo-comments colors

--- lua/modules/configs/tool/todo_comments.lua
+++ lua/modules/configs/tool/todo_comments.lua
@@ -7,16 +7,30 @@ return function()
 			FIX = {
 				icon = " ", -- icon used for the sign, and in search results
 				color = "error", -- can be a hex color, or a named color (see below)
-				alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR" }, -- a set of other keywords that all map to this FIX keywords
+				alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR", "CRITICAL" }, -- a set of other keywords that all map to this FIX keywords
 				-- signs = false, -- configure signs for some keywords individually
 			},
-			EXPLA = { icon = "", color = "explanation", alt = { "explain", "DESC" } },
-			CRITICAL = { icon = " ", color = "critical" },
-			TODO = { icon = " ", color = "info" },
+			EXPLA = { icon = "", color = "explanation", alt = { "explain", "DESC", "REFER" } },
+			TODO = { icon = " ", color = "todo" },
 			HACK = { icon = " ", color = "warning" },
 			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
 			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-			NOTE = { icon = " ", colr = "hint", alt = { "INFO" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO", "HINT" } },
+			TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+		},
+		colors = {
+			todo = { "#ffc125" },
+			explanation = { "#ABDC78" },
+			error = { "#FF0000" },
+			warning = { "#ffff00" },
+			info = { "#2563EB" },
+			hint = { "#2deaa4" },
+			default = { "#7C3AED" },
+			test = { "#61afef" },
+		},
+		gui_style = {
+			fg = "NONE", -- The gui style to use for the fg highlight group.
+			bg = "NONE", -- The gui style to use for the bg highlight group.
 		},
 		merge_keywords = true, -- when true, custom keywords will be merged with the defaults
 		-- highlighting of the line containing the todo comment
@@ -24,8 +38,11 @@ return function()
 		-- * keyword: highlights of the keyword
 		-- * after: highlights after the keyword (todo text)
 		highlight = {
+			multiline = false, -- enable multine todo comments
+			multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+			multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
 			before = "", -- "fg" or "bg" or empty
-			keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+			keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
 			after = "fg", -- "fg" or "bg" or empty
 			pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
 			comments_only = true, -- uses treesitter to match keywords in comments only
@@ -34,15 +51,6 @@ return function()
 		},
 		-- list of named colors where we try to extract the guifg from the
 		-- list of hilight groups or use the hex color if hl not found as a fallback
-		colors = {
-			explanation = { "#ABDC78" },
-			critical = { "#FF0000" },
-			error = { "#DC2626" },
-			warning = { "#FBBF24" },
-			info = { "#2563EB" },
-			hint = { "#10B981" },
-			default = { "#7C3AED" },
-		},
 		search = {
 			command = "rg",
 			args = {
