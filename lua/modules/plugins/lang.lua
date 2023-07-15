f50473e6895c580123a95076000a0286ba9aef25
Deg2v <1311618499@qq.com>
2023-07-15 15:44:05 +0800

    add comment neorg for orgmode, maybe use in future

--- lua/modules/plugins/lang.lua
+++ lua/modules/plugins/lang.lua
@@ -23,6 +23,26 @@ lang["nvim-orgmode/orgmode"] = {
 	ft = "org",
 	config = require("lang.orgmode"),
 }
+-- lang["nvim-neorg/neorg"] = {
+-- 	ft = "org",
+-- 	build = ":Neorg sync-parsers",
+-- 	dependencies = { "nvim-lua/plenary.nvim" },
+-- 	config = function()
+-- 		require("neorg").setup({
+-- 			load = {
+-- 				["core.defaults"] = {}, -- Loads default behaviour
+-- 				["core.concealer"] = {}, -- Adds pretty icons to your documents
+-- 				["core.dirman"] = { -- Manages Neorg workspaces
+-- 					config = {
+-- 						workspaces = {
+-- 							notes = "~/notes",
+-- 						},
+-- 					},
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+-- }
 -- lang["Saecki/crates.nvim"] = {
 -- 	lazy = true,
 -- 	event = "BufReadPost Cargo.toml",
