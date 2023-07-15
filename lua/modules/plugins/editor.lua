d9bbbec80c0e1016b7f66d664ce7d0aa439285a0
Deg2v <1311618499@qq.com>
2023-07-15 15:45:51 +0800

    replace leap flit with folke/flash.nvim

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -61,18 +61,31 @@ editor["junegunn/vim-easy-align"] = {
 -- 	event = { "CursorHold", "CursorHoldI" },
 -- 	config = require("editor.hop"),
 -- }
-editor["ggandor/leap.nvim"] = {
-	lazy = true,
-	event = "BufReadPost",
-	dependencies = { { "tpope/vim-repeat" } },
-	config = require("editor.leap"),
+editor["folke/flash.nvim"] = {
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {},
+  -- stylua: ignore
+    keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
 }
-editor["ggandor/flit.nvim"] = {
-	lazy = true,
-	event = { "BufReadPost", "BufAdd", "BufNewFile" },
-	dependencies = "leap.nvim",
-	config = require("editor.flit"),
-}
+-- editor["ggandor/leap.nvim"] = {
+-- 	lazy = true,
+-- 	event = "BufReadPost",
+-- 	dependencies = { { "tpope/vim-repeat" } },
+-- 	config = require("editor.leap"),
+-- }
+-- editor["ggandor/flit.nvim"] = {
+-- 	lazy = true,
+-- 	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+-- 	dependencies = "leap.nvim",
+-- 	config = require("editor.flit"),
+-- }
 editor["RRethy/vim-illuminate"] = {
 	lazy = true,
 	event = { "CursorHold", "CursorHoldI" },
