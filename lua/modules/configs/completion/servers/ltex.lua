c9972c5473b6d888a9ba7891cce05d04f76b8154
Deg2v <1311618499@qq.com>
2023-07-22 13:15:16 +0800

    correct ltex-ls config

--- lua/modules/configs/completion/servers/ltex.lua
+++ lua/modules/configs/completion/servers/ltex.lua
@@ -1,15 +1,17 @@
-return {
-	settings = {
-		-- use_spellfile = true,
-		ltex = {
-			-- enabled = {"latex", "tex", "bib", "markdown",},
-			disabledRules = {
-				-- ["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
-				-- ["zh-CN"] = { "MORFOLOGIK_RULE_EN_US" },
-			},
-			dictionary = {
-				["en-US"] = { ":" .. vim.fn.stdpath("data") .. "/spell/dictionary.txt" },
+return function()
+	require("ltex-ls").setup({
+		settings = {
+			-- use_spellfile = true,
+			ltex = {
+				-- enabled = {"latex", "tex", "bib", "markdown",},
+				disabledRules = {
+					-- ["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+					-- ["zh-CN"] = { "MORFOLOGIK_RULE_EN_US" },
+				},
+				dictionary = {
+					["en-US"] = { ":" .. vim.fn.stdpath("data") .. "/spell/dictionary.txt" },
+				},
 			},
 		},
-	},
-}
+	})
+end
