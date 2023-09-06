dd75d92683b18aa49abba8cac64999b764e24fd3
Deg2v <1311618499@qq.com>
2023-09-06 14:53:55 +0800

    for luacheck

--- lua/user/keymap/editor.lua
+++ lua/user/keymap/editor.lua
@@ -1,6 +1,5 @@
 local bind = require("keymap.bind")
 local map_cr = bind.map_cr
-local map_cmd = bind.map_cmd
 local addtitle = require("user.keymap.authorinfo").addtitle
 return {
 	-- ["n|q"] = map_cmd("<ESC>"):with_silent():with_desc("disable recording macro in q"), -- FIXME: work in core/mapping.lua, but not work here
