06b6ea27d5e744cc42bf53be7d24f59fabe5d539
Deg2v <1311618499@qq.com>
2023-09-06 14:50:38 +0800

    restore file in the user_template folder

--- lua/user_template/event.lua
+++ lua/user_template/event.lua
@@ -2,16 +2,6 @@ local definitions = {
 	-- Example
 	bufs = {
 		{ "BufWritePre", "COMMIT_EDITMSG", "setlocal noundofile" },
-		{
-			"BufEnter,FocusGained,WinEnter,InsertLeave",
-			"*",
-			[[if &number | set relativenumber | endif]],
-		},
-		{
-			"BufLeave,FocusLost,WinLeave,Insertenter",
-			"*",
-			[[if &number | set norelativenumber | endif]],
-		},
 	},
 }
 
--- lua/user_template/settings.lua
+++ lua/user_template/settings.lua
@@ -2,26 +2,9 @@
 local settings = {}
 
 settings["use_ssh"] = true
-settings["use_copilot"] = false
 settings["colorscheme"] = "onedark"
 
 -- Set the plugins to disable here.
 -- Example: "Some-User/A-Repo"
 ---@type string[]
-settings["disabled_plugins"] = {
-	-- ui
-	"Jint-lzxy/nvim",
-	"sainnhe/edge",
-	"nvim-lualine/lualine.nvim",
-	"shaunsingh/nord.nvim",
-	-- editor
-	"rhysd/clever-f.vim",
-	"smoka7/hop.nvim",
-	-- lang
-	"fatih/vim-go",
-	"simrat39/rust-tools.nvim",
-	"Saecki/crates.nvim",
-	-- tool
-	"gelguy/wilder.nvim",
-}
 return settings
